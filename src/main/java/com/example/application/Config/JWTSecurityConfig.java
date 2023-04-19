780b01c7308fccb46c7cfaf829f5cac7d4d6c8de
jstand <akiricuk37@gmail.com>
2023-04-19 23:48:28 +0200

    64% Progress

--- src/main/java/com/example/application/Config/JWTSecurityConfig.java
+++ src/main/java/com/example/application/Config/JWTSecurityConfig.java
@@ -50,7 +50,7 @@ public class JWTSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
+        http.addFilter(preAuthFilter())
                 .authorizeRequests()
                 .antMatchers("/auth/**").permitAll()
                 .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
